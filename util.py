ac4a49ffca42ac691fdf2b9780f91044aefb06ec
Liu Changyu <liuchangyu1111@gmail.com>
2020-10-17 09:07:25 +0800

    Update util.py

--- util.py
+++ util.py
@@ -211,10 +211,10 @@ def kernel_shift(kernel, sf):
 
 def save_final_kernel(k_2, conf):
     """saves the final kernel and the analytic kernel to the results folder"""
-    sio.savemat(os.path.join(conf.output_dir_path, '%s_kernel_x2.mat' % conf.img_name), {'Kernel': k_2})
+    sio.savemat(os.path.join(conf.output_dir_path, '%s_kernel_2x.mat' % conf.img_name), {'Kernel': k_2})
     if conf.X4:
         k_4 = analytic_kernel(k_2)
-        sio.savemat(os.path.join(conf.output_dir_path, '%s_kernel_x4.mat' % conf.img_name), {'Kernel': k_4})
+        sio.savemat(os.path.join(conf.output_dir_path, '%s_kernel_4x.mat' % conf.img_name), {'Kernel': k_4})
 
 
 def run_zssr(k_2, conf):
